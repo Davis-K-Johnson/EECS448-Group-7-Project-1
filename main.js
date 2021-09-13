dce2600c9228c550cd583d041506b9c36fa49a98
Curtis Braxdale <BraxdaleCurtis@gmail.com>
2021-09-12 21:38:41 -0500

    Aligned canvas, fixed drawGrid() method,

--- main.js
+++ main.js
@@ -43,97 +43,68 @@ Should it be you vs the cpu?
 
 let canvas;
 let context;
-let model = {
-    game_size: 6, // chosen by the player, denotes how many ships the game will have.
-    player_turn: 1, // determines which player's turn it is.
-    game_status: "active", // states to determine what to place on canvas (initial, active, win, lose)
 
-    // status of player_1's ships , true = afloat, false = sunk.
-    p1_ship1: true, p1_ship2: true, p1_ship3: true,
-    p1_ship4: true, p1_ship5: true, p1_ship6: true,
-
-    // status of player_2's ships , true = afloat, false = sunk.
-    p2_ship1: true, p2_ship2: true, p2_ship3: true,
-    p2_ship4: true, p2_ship5: true, p2_ship6: true,
-
-    // the key of each player that will be updated when each player places their ships.
-    // this will be used as a reference when each shot is taken to determine a hit or miss.
-    // will also be used to determine if an entire ship has sunk.
-    key_p1_1: "#XXX#######", key_p1_2: "###########", key_p1_3: "###########", 
-    key_p1_4: "###########", key_p1_5: "###########", key_p1_6: "###########",
-    key_p1_7: "###########", key_p1_8: "###########", key_p1_9: "###########",
-
-    key_p2_1: "###########", key_p2_2: "#####X#####", key_p2_3: "###########",
-    key_p2_4: "###########", key_p2_5: "###########", key_p2_6: "###########",
-    key_p2_7: "###########", key_p2_8: "###########", key_p2_9: "###########",
-
-    // the game board of each player that will be updated when each player takes a shot.
-    // this will keep track of the coordinates that have been shot at, with hits & misses.
-    game_p1_1: "###########", game_p1_2: "###########", game_p1_3: "###########",
-    game_p1_4: "###########", game_p1_5: "###########", game_p1_6: "###########",
-    game_p1_7: "###########", game_p1_8: "###########", game_p1_9: "###########",
-
-    game_p2_1: "###########", game_p2_2: "###########", game_p2_3: "###########",
-    game_p2_4: "###########", game_p2_5: "###########", game_p2_6: "###########",
-    game_p2_7: "###########", game_p2_8: "###########", game_p2_9: "###########",
-}
-
-// used to draw both the player 1 & player 2 grid.
+// used to draw all elements to the canvas
 function drawCanvas()
 {
-    if (model.game_status == "win") {
-        //drawWin();
-        return;
-    }
-    if (model.game_status == "lose") {
-        //drawLose();
-        return;
-    }
     drawGrid();
-    fillGrid(1);
-    fillGrid(2);
+    //fillGrid(1);
+    //fillGrid(2);
 }
 
 // used to draw the grid with which will contain information for the game.
 function drawGrid()
 {
     context.beginPath();
-    context.moveTo(175, 325); context.lineTo(675, 325); context.lineTo(675, 775);
-    context.lineTo(175, 775); context.lineTo(175, 325); context.moveTo(225, 325);
-    context.lineTo(225, 775); context.moveTo(275, 325); context.lineTo(275, 775);
-    context.moveTo(325, 325); context.lineTo(325, 775); context.moveTo(375, 325);
-    context.lineTo(375, 775); context.moveTo(425, 325); context.lineTo(425, 775);
-    context.moveTo(475, 325); context.lineTo(475, 775); context.moveTo(525, 325);
-    context.lineTo(525, 775); context.moveTo(575, 325); context.lineTo(575, 775);
-    context.moveTo(625, 325); context.lineTo(625, 775); context.moveTo(175, 375);
-    context.lineTo(675, 375); context.moveTo(175, 425); context.lineTo(675, 425);
-    context.moveTo(175, 475); context.lineTo(675, 475); context.moveTo(175, 525);
-    context.lineTo(675, 525); context.moveTo(175, 575); context.lineTo(675, 575);
-    context.moveTo(175, 625); context.lineTo(675, 625); context.moveTo(175, 675);
-    context.lineTo(675, 675); context.moveTo(175, 725); context.lineTo(675, 725);
+    
+    //draw the horizontal lines on the left grid
+    context.moveTo(100, 75);
+    for(var i = 0; i < 11; i++)
+    {
+        context.moveTo(100 + (i*65), 75);
+        context.lineTo(100 + (i*65), 75 + (9*65));
+    }
+    //draw the vertical lines on the left grid
+    context.moveTo(100, 75);
+    for(var i = 0; i < 10; i++)
+    {
+        context.moveTo(100 , 75 + (i*65));
+        context.lineTo(100 + (10*65), 75 + (i*65));
+    }
+    //draw the horizontal lines on the right grid
+    context.moveTo(1000, 75);
+    for(var i = 0; i < 11; i++)
+    {
+        context.moveTo(1000 + (i*65), 75);
+        context.lineTo(1000 + (i*65), 75 + (9*65));
+    }
+    //draw the vertical lines on the right grid
+    context.moveTo(1000, 75);
+    for(var i = 0; i < 10; i++)
+    {
+        context.moveTo(1000 , 75 + (i*65));
+        context.lineTo(1000 + (10*65), 75 + (i*65));
+    }
+
     context.strokeStyle = 'black';
     context.lineWidth = 2;
     context.stroke();
     context.closePath();
 
-    context.beginPath();
-    context.moveTo(875, 325); context.lineTo(1375, 325); context.lineTo(1375, 775);
-    context.lineTo(875, 775); context.lineTo(875, 325); context.moveTo(925, 325);
-    context.lineTo(925, 775); context.moveTo(975, 325); context.lineTo(975, 775);
-    context.moveTo(1025, 325); context.lineTo(1025, 775); context.moveTo(1075, 325);
-    context.lineTo(1075, 775); context.moveTo(1125, 325); context.lineTo(1125, 775);
-    context.moveTo(1175, 325); context.lineTo(1175, 775); context.moveTo(1225, 325);
-    context.lineTo(1225, 775); context.moveTo(1275, 325); context.lineTo(1275, 775);
-    context.moveTo(1325, 325); context.lineTo(1325, 775); context.moveTo(875, 375);
-    context.lineTo(1375, 375); context.moveTo(875, 425); context.lineTo(1375, 425);
-    context.moveTo(875, 475); context.lineTo(1375, 475); context.moveTo(875, 525);
-    context.lineTo(1375, 525); context.moveTo(875, 575); context.lineTo(1375, 575);
-    context.moveTo(875, 625); context.lineTo(1375, 625); context.moveTo(875, 675);
-    context.lineTo(1375, 675); context.moveTo(875, 725); context.lineTo(1375, 725);
-    context.strokeStyle = 'black';
-    context.lineWidth = 2;
-    context.stroke();
-    context.closePath();
+    //set the column indicators above the grids
+    let column = "ABCDEFGHIJ";
+    for(var k = 0; k < 11; k++)
+    {
+        context.fillText(column.charAt(k), 125 + (k*65), 60);
+        context.fillText(column.charAt(k), 1025 + (k*65), 60);
+    }
+
+    //set the row indicators beside the grids
+    for(var k = 0; k < 9; k++)
+    {
+        context.fillText(k + 1, 65, 115 + (k*65));
+        context.fillText(k + 1, 965, 115 + (k*65));
+    }
 }
 
 // used to fill the grid of either player with updated indicators.
@@ -143,27 +114,11 @@ function fillGrid(player)
     {
         if(player == 1)
         {
-            context.fillText(model.key_p1_1.charAt(i),193 +(i*50), 355);
-            context.fillText(model.key_p1_2.charAt(i),193 +(i*50), 405);
-            context.fillText(model.key_p1_3.charAt(i),193 +(i*50), 455);
-            context.fillText(model.key_p1_4.charAt(i),193 +(i*50), 505);
-            context.fillText(model.key_p1_5.charAt(i),193 +(i*50), 555);
-            context.fillText(model.key_p1_6.charAt(i),193 +(i*50), 605);
-            context.fillText(model.key_p1_7.charAt(i),193 +(i*50), 655);
-            context.fillText(model.key_p1_8.charAt(i),193 +(i*50), 705);
-            context.fillText(model.key_p1_9.charAt(i),193 +(i*50), 755);
+
         }
         if(player == 2)
         {
-            context.fillText(model.key_p2_1.charAt(i),893 +(i*50), 355);
-            context.fillText(model.key_p2_2.charAt(i),893 +(i*50), 405);
-            context.fillText(model.key_p2_3.charAt(i),893 +(i*50), 455);
-            context.fillText(model.key_p2_4.charAt(i),893 +(i*50), 505);
-            context.fillText(model.key_p2_5.charAt(i),893 +(i*50), 555);
-            context.fillText(model.key_p2_6.charAt(i),893 +(i*50), 605);
-            context.fillText(model.key_p2_7.charAt(i),893 +(i*50), 655);
-            context.fillText(model.key_p2_8.charAt(i),893 +(i*50), 705);
-            context.fillText(model.key_p2_9.charAt(i),893 +(i*50), 755);
+
         }
     }
 }
@@ -179,12 +134,6 @@ function refresh(n) {
     drawCanvas();
     tick();
   }
-
-document.addEventListener("DOMContentLoaded", () => { 
-    canvas = document.querySelector("#gameCanvas");
-    context = canvas.getContext("2d");
-    refresh();
-  })
 
 document.addEventListener("DOMContentLoaded", () => { 
     canvas = document.querySelector("#gameCanvas");
